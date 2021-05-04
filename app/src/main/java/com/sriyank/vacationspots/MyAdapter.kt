055cc04c2813e91e57bd2dec7da52d65f3cc9354
Deepak <deepakrout@hotmail.com>
2021-05-04 13:32:49 -0400

    Initial commit

---
+++ app/src/main/java/com/sriyank/vacationspots/MyAdapter.kt
@@ -0,0 +1,79 @@
+package com.sriyank.vacationspots
+
+import android.content.Context
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.list_item.view.*
+
+class MyAdapter(val context: Context, var destinationList: ArrayList<Destination>): RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
+
+    inner class MyViewHolder (itemView: View): RecyclerView.ViewHolder(itemView), View.OnClickListener {
+
+        var currentPosition: Int        = -1
+        var destination: Destination?   = null
+
+
+        fun setData(destination: Destination, position: Int) {
+            itemView.apply {
+                txvPlaceName.text = destination.placeName
+                imvPlace.setImageResource(destination.imageId)
+            }
+
+            this.currentPosition = position
+            this.destination = destination
+
+        }
+
+        fun setListeners() {
+            itemView.apply {
+                imvDelete.setOnClickListener(this@MyViewHolder)
+                imvMakeCopy.setOnClickListener(this@MyViewHolder)
+            }
+        }
+
+        override fun onClick(v: View?) {
+
+            when (v!!.id) {
+                R.id.imvDelete -> deleteItem()
+                R.id.imvMakeCopy -> addItem()
+            }
+        }
+
+        fun deleteItem() {
+
+            destinationList.removeAt(currentPosition)
+            notifyItemRemoved(currentPosition)
+            notifyItemRangeChanged(currentPosition, destinationList.size)
+        }
+
+        fun addItem() {
+
+            destinationList.add(currentPosition, destination!!)
+            notifyItemInserted(currentPosition)
+            notifyItemRangeChanged(currentPosition, destinationList.size)
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+
+        Log.i("MyAdapter", "onCreateViewHolder")
+        val itemView = LayoutInflater.from(context).inflate(R.layout.list_item,parent,false)
+        return MyViewHolder(itemView)
+    }
+
+    override fun getItemCount(): Int = destinationList.size
+
+    override fun onBindViewHolder(itemViewHolder: MyViewHolder, position: Int) {
+
+        Log.i("MyAdapter", "onBindViewHolder $position")
+
+        val destination = destinationList[position]
+        itemViewHolder.setData(destination,position)
+
+        itemViewHolder.setListeners()
+    }
+}
